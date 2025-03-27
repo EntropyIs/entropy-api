18aec691b19462c137114b3b479edb01bfa61ec2
James P. Galovic <jpgalovic@gmail.com>
2025-03-28 04:26:18 +1100

    ci(devtools): Added Zod OpenAPI and changed entry point to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
 import { Hono } from "hono";
 
-import { renderer } from "./renderer";
-
 const app = new Hono();
 
-app.use(renderer);
-
 app.get("/", (c) => {
-  return c.render(<h1>Hello!</h1>);
+  return c.json({ message: "Hello, World!" });
 });
 
 export default app;
